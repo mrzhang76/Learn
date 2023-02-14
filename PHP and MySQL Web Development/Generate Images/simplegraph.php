8a4ae35d472a40544021d55cd6ffa953a918db42
mrzhang76 <zhangand76@outlook.com>
2023-02-15 00:10:52 +0800

    zlib and pnglib

---
+++ PHP and MySQL Web Development/Generate Images/simplegraph.php
@@ -0,0 +1,17 @@
+<?php 
+    $height = 200;
+    $width = 200;
+
+    $im = imagecreatetruecolor($width,$height);
+    $white = imagecolorallocate($im, 255, 255, 255);
+    $blue = imagecolorallocate($im, 0, 0, 255);
+
+    imagefill($im, 0, 0, $blue);
+    imageline($im, 0, 0, $width, $height, $white);
+    imagestring($im, 4, 50, 150, 'Sales', $white);
+
+    header('Content-type: image/png');
+    imagepng($im);
+
+    imagedestroy($im);
+?>
