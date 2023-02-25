9589c1f31401444162fd89d36aca7699ba2785b2
mrzhang76 <zhangand76@outlook.com>
2023-02-25 22:24:27 +0800

    phpbookmark

---
+++ PHP and MySQL Web Development/PHPbookmark/bookmarks.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE bookmarks;
+USE bookmarks;
+
+CREATE TABLE user(
+    username VARCHAR(16) NOT NULL PRIMARY KEY,
+    passwd CHAR(40) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE bookmarks(
+    username VARCHAR(16) NOT NULL,
+    bm_URL VARCHAR(256) NOT NULL,
+    INDEX (username),
+    INDEX (bm_URL),
+    PRIMARY KEY (username, bm_URL)
+);
+
+create user 'bm_user'@'localhost' identified by 'password';
+grant select,insert,update,delete,index,create,drop on bookmarks.* to 'bm_user'@'localhost'; 
