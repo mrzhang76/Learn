343b39cc2cdd30ddb3e1088d89aa8e4e41a94cca
mrzhang76 <zhangand76@outlook.com>
2023-02-27 23:48:54 +0800

    finish output_fns

--- PHP and MySQL Web Development/PHPbookmark/output_fns.php
+++ PHP and MySQL Web Development/PHPbookmark/output_fns.php
@@ -29,4 +29,239 @@ function do_html_header($title){
     </html>
     <?php
 }
+
+function do_html_footer(){
+    ?>
+    </body>
+    </html>
+<?php
+}
+
+function do_html_heading($heading){
+    ?>
+    <h2><?php echo $heading;?></h2>
+    <?php 
+}
+
+function do_html_URL($url, $name){
+    ?>
+    <br /><a href="<?php echo $url;?>"><?php echo $name;?></a><br />
+    <?php 
+}
+
+function display_site_info(){
+    ?>
+    <ul>
+        <li>Store your bookmarks online with us!</li>
+        <li>See what other users use!</li>
+        <li>Share your favorite links with others!</li>
+    </ul>
+    <?php
+}
+
+function display_login_form(){
+    ?>
+    <p><a href="register_form.php">Not a member?</a></p>
+    <form method="post" action="member.php">
+        <div class="formblock">
+            <h2>Members Log In Here</h2>
+            <p>
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="passwd">
+            </p>
+            <p>
+                <label for="passwd">Password:</label>
+                <input type="password" name="passwd" id="passwd" />
+            </p>
+            <button type="submit">Log In</button>
+            <p><a href="forgot_form.php">Forgot your password</a></p>
+        </div>
+    </form>
+    <?php
+}
+
+function display_registration_form(){
+    ?>
+    <form method="post" action="register_new.php">
+        <div class="formblock">
+            <h2>Register Now</h2>
+            <p>
+                <label for="email">Email Address:</label><br />
+                <input type="email" name="email" id="email" size="30" maxlength="100" required/>
+            </p>
+            <p>
+                <label for="username">Preferred Username<br />(max 16 chars):</label><br />
+                <input type="text" name="username" id="username" size="16" maxlength="16" required/>
+            </p>
+            <p>
+                <label for="passwd">Password <br>(between 6 and 16 chars):</label><br />
+                <input type="password" name="passwd" id="passwd" size="16" maxlength="16" required/>
+            </p>
+            <p>
+                <label for="passwd2">Confirm Passwd:</label><br />
+                <input type="password" name="passwd2" id="passwd2" size="16" maxlength="16" required/>
+            </p>
+            <button type="submit">Register</button>
+        </div>
+    </form>
+    <?php 
+}
+
+function display_user_urls($url_array){
+    global $bm_table;
+    $bm_table;
+    ?>
+    <br>
+    <form name="bm_table" action="delete_bms.php" method="post">
+        <table width="600" cellpadding="2" cellspacing="0">
+    <?php
+    $color = "#cccccc";
+    echo "<tr bgcolor=\"".$color."\">
+            <td>
+                <strong>Bookmark</strong>
+            </td>
+            <td>
+                <strong>Title</strong>
+            </td>
+            <td>
+                <strong>Description</strong>
+            </td>  
+            <td>
+                <strong>Delete?</strong>      
+            </td>
+        </tr>";
+    if((is_array($url_array)) && (count($url_array) > 0)){
+        foreach($url_array as $url)
+            if($color == "#cccccc")
+                $color = "#ffffff";
+            else
+                $color = "#cccccc";
+        $urlString = $url[0];
+        $titleString = $url[1];
+        $descriptionString = $url[2];
+        echo "<tr bgcolor=\"".$color."\">
+                <td>
+                    <a href=\"".$urlString."\">".htmlspecialchars($urlString)."</a>
+                </td>
+                <td>".htmlspecialchars($titleString)."</td>
+                <td>".htmlspecialchars($descriptionString)."</td>
+                <td>
+                    <input type=\"checkbox\" name=\"del_me[]\" value=\"".$urlString."\">
+                </td>
+            </tr>";
+    }
+    else
+        echo "<tr><td>No bookmarks on record.</td></tr>";
+    ?>
+        </table>
+    </form>
+<?php   
+}
+
+function display_user_menu(){
+    ?>
+    <hr>
+    <a href="member.php">Home</a>  &nbsp;|&nbsp;
+    <a href="add_bm_form.php">Add BM</a>  &nbsp;|&nbsp;
+    <?php 
+    global $bm_table;
+    if($bm_table == true)
+        echo "<a href=\'#\" onclick=\"bm_table.submit();\">Delete BM</a> &nbsp;|&nbsp;";
+    else
+        echo "<span style=\"color:#cccccc\">Delete BM</span>  &nbsp;|&nbsp;";
+    ?>
+    <a href="change_passwd_form.php">Change password</a><br>
+    <a href="recommend.php">Recommend URLs to me</a> &nbsp;|&nbsp;
+    <a href="logout.php">Logout</a>
+    <hr>
+    <?php
+}
+
+function display_add_bm_form(){
+    ?>
+    <form name="bm_table" action="add_bms.php" method="post">
+        <div class="formblock">
+            <h2>New Bookmark</h2>
+            <p>
+                <input type="text" name="new_url" id="new_url" size="40" maxlength="255"
+                    value="http://" required />
+            </p>
+            <button type="submit">Add Bookmark</button>
+        </div>
+    </form>
+    <?php
+}
+
+function display_password_form(){
+    ?>
+    <br>
+    <form action="change_passwd.php" method="post">
+        <div class="formlock">
+            <h2>Change Password</h2>
+            <p>
+                <label for="old_passwd">Old Password:</label><br />
+                <input type="password" name="old_passwd" id="old_passwd" size="16" maxlength="16" required />
+            </p>
+            <p>
+                <label for="passwd">New Password:</label><br />
+                <input type="password" name="new_passwd" id="new_passwd" size="16" maxlength="16" required />
+            </p>
+            <p>
+                <label for="passwd2">Repeat New Password:</label><br />
+                <input type="password" name="new_passwd2" id="new_passwd2" size="16" maxlength="16" required />
+            </p>
+            <button type="submit">Change Password</button>
+        </div>
+        <br>
+    </form>
+    <?php
+}
+
+function display_forgot_form(){
+    ?>
+    <br>
+    <form action="forgot_passwd.php" method="post">
+        <div class="formblock">
+            <h2>Forgot Your Password?</h2>
+            <p>
+                <label for="username">Enter Your Username:</label><br />
+                <input type="text" name="username" id="username" size="16" maxlength="16" required />
+            </p>
+            <button type="submit">Change Password</button>
+        </div>
+        <br>
+    </form>
+    <?php
+}
+
+function display_recommended_urls($url_array){
+    ?>
+    <br>
+    <table width="300" cellpadding="2" cellspacing="0">
+    <?php
+    $color = "#cccccc";
+    echo "<tr bgcolor=\"".$color."\">
+            <td>
+                <strong>Recommendations</strong>
+            </td>  
+          </tr>";
+    if((is_array($url_array)) && (count($url_array) > 0)){
+        foreach($url_array as $url)
+            if($color == "#cccccc")
+                $color = "#ffffff";
+            else
+                $color = "#cccccc";
+        echo "<tr bgcolor=\"".$color."\">
+                <td>
+                    <a href=\"".$url."\">".htmlspecialchars($url)."</a>
+                </td>
+              </tr>";
+    }
+    else
+        echo "<tr><td>No recommendations for you today.</td></tr>";
+    ?>
+    </table>
+    <?php
+}
+
 ?>
--- PHP and MySQL Web Development/PHPbookmark/register_form.php
+++ PHP and MySQL Web Development/PHPbookmark/register_form.php
@@ -1,6 +1,6 @@
 <?php 
 require_once('bookmark_fns.php');
 do_html_header('User Registration');
-display_reistation_form();
+display_registration_form();
 do_html_footer();
 ?>
