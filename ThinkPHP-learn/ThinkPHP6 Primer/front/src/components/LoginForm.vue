f24110316f902094a5f27fa62850cf9dcb249a8b
mrzhang76 <zhangand76@outlook.com>
2023-03-05 14:24:31 +0800

    update

---
+++ ThinkPHP-learn/ThinkPHP6 Primer/front/src/components/LoginForm.vue
@@ -0,0 +1,15 @@
+<template>
+<v-main>
+    <v-container>
+        <h1>Login</h1>
+        <v-text-field label="email" type="email"></v-text-field>
+        <v-text-field label="passwd" type="password"></v-text-field>
+    <v-btn rounded>Login</v-btn>
+    </v-container>
+</v-main>
+</template>
+<script>
+export default {
+  name: 'LoginForm'
+}
+</script>
